16cc4b23beac4bdddd389d38fb1f2faccba20cd6
Shinji Ikeda <ike_s@ip-192-168-30-10.ap-northeast-1.compute.internal>
2015-02-13 17:37:34 +0900

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='python-sfor',
-    version='0.1',
+    version='0.1.1',
     author='shinjiikeda',
     author_email='gm.ikeda@gmail.com',
     packages=['sfor'],
