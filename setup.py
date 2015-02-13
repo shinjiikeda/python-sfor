8bebaebfcf62a6b5db3e90e5f3c2833346229195
Shinji Ikeda <ike_s@ip-192-168-30-10.ap-northeast-1.compute.internal>
2015-02-13 17:53:51 +0900

    update

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='python-sfor',
-    version='0.1.1',
+    version='0.1.3',
     author='shinjiikeda',
     author_email='gm.ikeda@gmail.com',
     packages=['sfor'],
     url='http://pypi.python.org/pypi/python-sfor',
     license='LICENSE.txt',
     description='Simple Failover Resolver',
-    long_description=open('README.md').read(),
+    long_description=open('README.txt').read(),
 )
